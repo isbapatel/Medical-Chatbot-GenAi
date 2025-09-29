673b8a75ee01452456b9e2f6404246966e909f18
isbapatel <isbapatel1205@gmail.com>
2025-09-29 19:24:13 +0530

    Added setup.py fix

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical-Chatbot-GenAi',
+    version= '0.0.1',
+    author= 'Isba Patel',
+    author_email= 'isbapatel1205@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
